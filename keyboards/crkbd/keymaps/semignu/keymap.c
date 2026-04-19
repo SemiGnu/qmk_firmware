85a8a12a9987c345e97004879a5fd5451687b3de
Tostein Winjum <twinjum@hotmail.com>
2026-04-19 22:40:22 +0200

    embetter keymap

--- keyboards/crkbd/keymaps/semignu/keymap.c
+++ keyboards/crkbd/keymaps/semignu/keymap.c
@@ -34,11 +34,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        _______,         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_UNDS,                    KC_PLUS, KC_7,    KC_8,    KC_9,    KC_MINS, _______,
+        _______,         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_UNDS,                 KC_PLUS, KC_7,    KC_8,    KC_9,    KC_ASTR, LC_DEL,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        _______,         KC_HASH, KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR,                  KC_EQL,  KC_4,    KC_5,    KC_6,    KC_KP_ASTERISK, KC_GRV,
+        _______,         KC_HASH, KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR,                  KC_EQL,  KC_4,    KC_5,    KC_6,    KC_MINS, KC_GRV,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        _______,         KC_3,    KC_E,    KC_1,    KC_Q,    XXXXXXX,                 KC_0,    KC_1,    KC_2,    KC_3,    KC_BACKSLASH, _______,
+        _______,         KC_3,    KC_E,    KC_1,    KC_Q,    XXXXXXX,                 KC_0,    KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
         //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                       _______, MO(3), _______,          _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_split_3x6_3(
         //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        _______,         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_UNDS,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        _______,         KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_UNDS,                 KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         _______,         KC_HASH, KC_CIRC, KC_LPRN, KC_RPRN, KC_DLR,                  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
         //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
